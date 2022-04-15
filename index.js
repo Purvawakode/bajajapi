c21cfc817e666a4e45068d6f1e2021e94000c4d7
Purvawakode <purvawakode2229@gmail.com>
2022-04-15 13:13:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express")
+const bodyParser = require('body-parser')
+
+let port = 3000;
+const app = express()
+app.use(bodyParser.json());
+app.listen(port, () => {
+    console.log(`App running on ${port}, http://localhost:${port}`)
+})
+
+app.post("/bfhl", (req, res) => {
+    console.log(req.body.data)
+    let arra = req.body.data
+    let num = [];
+    let alphabet =[];
+    // Change the user id as per user name and DOB
+    let user_id = 'purva_wakode_22052001'
+    let email=`purvawakode2229@gmail.com`
+    let rollNo = `0827IT191089`
+    // Check whether the given array contains numeric elements or not
+    let stat=false
+    arra.forEach((ele) => {
+        // Check if the number is even or odd
+        if(typeof(ele)==="number"){
+            console.log(`${ele} is number`)
+            num.push(ele)
+            stat=true
+        } else {
+            console.log(`${ele} is alphabet`)
+            alphabet.push(ele)
+            stat=true
+        }
+        
+
+    })
+    if(stat){
+        res.status(200).json({
+            "is_success": stat,
+            "user_id": user_id,
+            "email": email,
+            "roll_number":rollNo,
+            "numbers": num,
+            "alphabets": alphabet
+        })
+    } else {
+        res.status(200).json({
+            "is_success": stat,
+            "user_id": user_id
+        })
+
+    }
+})
